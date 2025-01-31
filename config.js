00698bfd6e547309dc2c80723160098d60de1c8f
taivippro123 <phanvothanhtai1007@gmail.com>
2025-02-01 01:14:57 +0700

    delete fpt api

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ require('dotenv').config();
 module.exports = {
   SEPAY_API_URL: 'https://my.sepay.vn/userapi/transactions/list',
   SEPAY_API_TOKEN: process.env.SEPAY_API_TOKEN, // API Token từ biến môi trường
-  FPT_API_KEY: process.env.FPT_API_KEY,  // API Key của FPT AI từ biến môi trường
   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY // API Key của Google Cloud từ biến môi trường
 };
